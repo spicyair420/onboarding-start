d79c4d47327ca51bca74aa001c9987675a0688aa
spicyair420 <arshvirghotra1@gmail.com>
2026-09-21 19:54:33 +0000

    worky?

--- src/project.v
+++ src/project.v
@@ -17,8 +17,8 @@ module tt_um_uwasic_onboarding_Arshvir_Ghotra (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
   assign uio_oe  = 8'hFF;
 
   wire [7:0] en_reg_out_7_0;
